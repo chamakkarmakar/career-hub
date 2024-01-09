fa8e6feb11752a49a9a495da8588b7d683eb9b7b
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-01-09 14:51:49 +0600

    make responsive the application form

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -18,7 +18,7 @@ const Modal = ({ setShowModal, id }) => {
             <div
                 className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             >
-                <div className="relative my-6 mx-auto w-1/3" >
+                <div className="relative my-6 mx-auto md:w-1/3 w-3/4" >
                     {/*content*/}
                     <div className="border rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                         {/*header*/}
